31be93ee66ee366146ad38eb666c6a255d305f0a
aeolkk14 <jeonej0514@gmail.com>
2023-01-26 16:49:20 +0900

    feat: Comment & Detail page

--- shinhantoy/order/views.py
+++ shinhantoy/order/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics, mixins, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import Order
+from .models import Order, Comment
 from .paginations import OrderLargePagination
-from .serializers import OrderSerializer
+from .serializers import OrderSerializer, CommentSerializer, CommentCreateSerializer
 
 # Create your views here.
 
@@ -21,3 +21,56 @@ class OrderListView(
 
     def get(self, request, *args, **kwargs):
         return self.list(request, args, kwargs)
+
+class OrderDetailView(
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    generics.GenericAPIView
+):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return Order.objects.order_by('-id')
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, args, kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, args, kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.partial_update(request, args, kwargs)
+
+class CommentListView(
+    mixins.ListModelMixin,
+    generics.GenericAPIView
+): 
+    serializer_class = CommentSerializer
+    
+
+    def get_queryset(self):
+        order_id=self.kwargs.get('order_id')
+        if order_id:
+            
+            return Comment.objects.filter(order_id=order_id) \
+                    .select_related('member', 'order') \
+                    .order_by('-id') 
+        return Comment.objects.none()
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, args, kwargs)
+
+
+class CommentCreateView(
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+):
+    serializer_class = CommentCreateSerializer
+    
+    def get_queryset(self): 
+        return Comment.objects.all()
+    
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, args, kwargs)
