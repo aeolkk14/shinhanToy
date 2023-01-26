17c38062b7ed5ed99c469505b3fe70eff66f354d
aeolkk14 <jeonej0514@gmail.com>
2023-01-26 13:47:23 +0900

    feat: Create change_password html file & Connect to backend

--- shinhantoy/member/urls.py
+++ shinhantoy/member/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.MemberRegisterView.as_view())
+    path("", views.MemberRegisterView.as_view()),
+    path("/password", views.MemberChangePasswordView.as_view())
+    
 ]
--- shinhantoy/member/views.py
+++ shinhantoy/member/views.py
@@ -1,5 +1,12 @@
-from rest_framework import mixins, generics
+from rest_framework import mixins, generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from django.contrib.auth.hashers import check_password, make_password
+
 from .serializers import MemberSerializer
+from .models import Member
 
 # Create your views here.
 
@@ -13,3 +20,29 @@ class MemberRegisterView(
 
     def post(self, request, *args, **kwargs):
         return self.create(request, args, kwargs)
+
+
+    
+class MemberChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, *Args, **kwargs):
+        current = request.data.get('current')
+        password1 = request.data.get('password1')
+        password2 = request.data.get('password2')
+
+        if password1 != password2:
+            return Response({
+                'detail': 'Wrong new password',
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        member = request.user
+        if not check_password(current, member.password):
+            return Response({
+                'detail': 'Wrong new password',
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        member.password = make_password(password1)
+        member.save()
+
+        return Response(status=status.HTTP_200_OK)
